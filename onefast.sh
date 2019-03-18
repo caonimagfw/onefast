a3edfdc8a1e780734755cca0be199ac74c0b6066
UXH <24637370+uxh@users.noreply.github.com>
2019-03-18 15:08:51 +0800

    初始化

--- onefast.sh
+++ onefast.sh
@@ -23,6 +23,106 @@ Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 
+####检查系统类型####
+function checkRelease(){
+	if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+}
+
+#####检查系统版本####
+function checkReleaseVersion(){
+	if [[ -s /etc/redhat-release ]]; then
+		version=`grep -oE  "[0-9.]+" /etc/redhat-release | cut -d . -f 1`
+	else
+		version=`grep -oE  "[0-9.]+" /etc/issue | cut -d . -f 1`
+	fi
+	bit=`uname -m`
+	if [[ ${bit} = "x86_64" ]]; then
+		bit="x64"
+	else
+		bit="x32"
+	fi
+}
+
+####检查加速状态####
+function checkStatus(){
+	kernel_version=`uname -r | awk -F "-" '{print $1}'`
+	kernel_version_full=`uname -r`
+	if [[ ${kernel_version_full} == "4.14.91-bbrplus" ]]; then
+		kernel_status="BBRplus"
+	elif [[ ${kernel_version} == "3.10.0" || ${kernel_version} == "3.16.0" || ${kernel_version} == "3.2.0" || ${kernel_version} == "4.4.0" || ${kernel_version} == "3.13.0"  || ${kernel_version} == "2.6.32" ]]; then
+		kernel_status="Lotserver"
+	elif [[ `echo ${kernel_version} | awk -F'.' '{print $1}'` == "4" ]] && [[ `echo ${kernel_version} | awk -F'.' '{print $2}'` -ge 9 ]]; then
+		kernel_status="BBR"
+	else 
+		kernel_status="noinstall"
+	fi
+
+	if [[ ${kernel_status} == "Lotserver" ]]; then
+		if [[ -e /appex/bin/serverSpeeder.sh ]]; then
+			run_status=`bash /appex/bin/serverSpeeder.sh status | grep "ServerSpeeder" | awk  '{print $3}'`
+			if [[ ${run_status} = "running!" ]]; then
+				run_status="启动成功"
+			else 
+				run_status="启动失败"
+			fi
+		else 
+			run_status="未安装加速模块"
+		fi
+	elif [[ ${kernel_status} == "BBR" ]]; then
+		run_status=`grep "net.ipv4.tcp_congestion_control" /etc/sysctl.conf | awk -F "=" '{print $2}'`
+		if [[ ${run_status} == "bbr" ]]; then
+			run_status=`lsmod | grep "bbr" | awk '{print $1}'`
+			if [[ ${run_status} == "tcp_bbr" ]]; then
+				run_status="BBR启动成功"
+			else 
+				run_status="BBR启动失败"
+			fi
+		elif [[ ${run_status} == "tsunami" ]]; then
+			run_status=`lsmod | grep "tsunami" | awk '{print $1}'`
+			if [[ ${run_status} == "tcp_tsunami" ]]; then
+				run_status="BBR魔改版启动成功"
+			else 
+				run_status="BBR魔改版启动失败"
+			fi
+		elif [[ ${run_status} == "nanqinlang" ]]; then
+			run_status=`lsmod | grep "nanqinlang" | awk '{print $1}'`
+			if [[ ${run_status} == "tcp_nanqinlang" ]]; then
+				run_status="暴力BBR魔改版启动成功"
+			else 
+				run_status="暴力BBR魔改版启动失败"
+			fi
+		else 
+			run_status="未安装加速模块"
+		fi
+	elif [[ ${kernel_status} == "BBRplus" ]]; then
+		run_status=`grep "net.ipv4.tcp_congestion_control" /etc/sysctl.conf | awk -F "=" '{print $2}'`
+		if [[ ${run_status} == "bbrplus" ]]; then
+			run_status=`lsmod | grep "bbrplus" | awk '{print $1}'`
+			if [[ ${run_status} == "tcp_bbrplus" ]]; then
+				run_status="BBRplus启动成功"
+			else 
+				run_status="BBRplus启动失败"
+			fi
+		else 
+			run_status="未安装加速模块"
+		fi
+	fi
+}
+
 #安装BBR内核
 installbbr(){
 	kernel_version="4.11.8"
@@ -259,7 +359,7 @@ startlotserver(){
 	fi
 	wget --no-check-certificate -O appex.sh https://raw.githubusercontent.com/0oVicero0/serverSpeeder_Install/master/appex.sh && chmod +x appex.sh && bash appex.sh install
 	rm -f appex.sh
-	start_menu
+	mainControl
 }
 
 #卸载全部加速
@@ -360,7 +460,7 @@ net.ipv4.ip_forward = 1">>/etc/sysctl.conf
 Update_Shell(){
 	echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
 	sh_new_ver=$(wget --no-check-certificate -qO- "http://${github}/tcp.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
-	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && start_menu
+	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && mainControl
 	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
 		echo -e "发现新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
 		read -p "(默认: y):" yn
@@ -377,86 +477,7 @@ Update_Shell(){
 	fi
 }
 
-#开始菜单
-start_menu(){
-clear
-echo && echo -e " TCP加速 一键安装管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
-  
- ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
-————————————内核管理————————————
- ${Green_font_prefix}1.${Font_color_suffix} 安装 BBR 内核
- ${Green_font_prefix}2.${Font_color_suffix} 安装 BBRmod 内核
- ${Green_font_prefix}3.${Font_color_suffix} 安装 BBRplus 内核 
- ${Green_font_prefix}4.${Font_color_suffix} 安装 Lotserver 内核[锐速]
-————————————加速管理————————————
- ${Green_font_prefix}5.${Font_color_suffix} 开启 BBR 加速
- ${Green_font_prefix}6.${Font_color_suffix} 开启 BBRmod 加速
- ${Green_font_prefix}7.${Font_color_suffix} 开启 BBRmod Pro 加速[不支持部分系统]
- ${Green_font_prefix}8.${Font_color_suffix} 开启 BBRplus 加速
- ${Green_font_prefix}9.${Font_color_suffix} 开启 Lotserver 加速[锐速]
-————————————杂项管理————————————
- ${Green_font_prefix}10.${Font_color_suffix} 卸载全部加速
- ${Green_font_prefix}11.${Font_color_suffix} 优化系统配置
- ${Green_font_prefix}12.${Font_color_suffix} 退出脚本
-————————————————————————————————" && echo
 
-	check_status
-	if [[ ${kernel_status} == "noinstall" ]]; then
-		echo -e " 当前状态: ${Green_font_prefix}未安装${Font_color_suffix} 加速内核 ${Red_font_prefix}请先安装内核${Font_color_suffix}"
-	else
-		echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} ${_font_prefix}${kernel_status}${Font_color_suffix} 加速内核 , ${Green_font_prefix}${run_status}${Font_color_suffix}"
-		
-	fi
-echo
-read -p " 请输入数字 [0-11]:" num
-case "$num" in
-	0)
-	Update_Shell
-	;;
-	1)
-	check_sys_bbr
-	;;
-	2)
-	check_sys_bbrmod
-	;;
-	3)
-	check_sys_bbrplus
-	;;
-	4)
-	check_sys_Lotsever
-	;;
-	5)
-	startbbr
-	;;
-	6)
-	startbbrmod
-	;;
-	7)
-	startbbrmod_nanqinlang
-	;;
-	8)
-	startbbrplus
-	;;
-	9)
-	startlotserver
-	;;
-	10)
-	remove_all
-	;;
-	11)
-	optimizing_system
-	;;
-	12)
-	exit 1
-	;;
-	*)
-	clear
-	echo -e "${Error}:请输入正确数字 [0-12]"
-	sleep 5s
-	start_menu
-	;;
-esac
-}
 #############内核管理组件#############
 
 #删除多余内核
@@ -519,43 +540,12 @@ BBR_grub(){
 
 #############系统检测组件#############
 
-#检查系统
-check_sys(){
-	if [[ -f /etc/redhat-release ]]; then
-		release="centos"
-	elif cat /etc/issue | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-	elif cat /proc/version | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /proc/version | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-    fi
-}
 
-#检查Linux版本
-check_version(){
-	if [[ -s /etc/redhat-release ]]; then
-		version=`grep -oE  "[0-9.]+" /etc/redhat-release | cut -d . -f 1`
-	else
-		version=`grep -oE  "[0-9.]+" /etc/issue | cut -d . -f 1`
-	fi
-	bit=`uname -m`
-	if [[ ${bit} = "x86_64" ]]; then
-		bit="x64"
-	else
-		bit="x32"
-	fi
-}
+
+
 
 #检查安装bbr的系统要求
 check_sys_bbr(){
-	check_version
 	if [[ "${release}" == "centos" ]]; then
 		if [[ ${version} -ge "6" ]]; then
 			installbbr
@@ -580,7 +570,6 @@ check_sys_bbr(){
 }
 
 check_sys_bbrmod(){
-	check_version
 	if [[ "${release}" == "centos" ]]; then
 		if [[ ${version} -ge "6" ]]; then
 			installbbrmod
@@ -605,7 +594,6 @@ check_sys_bbrmod(){
 }
 
 check_sys_bbrplus(){
-	check_version
 	if [[ "${release}" == "centos" ]]; then
 		if [[ ${version} -ge "6" ]]; then
 			installbbrplus
@@ -632,7 +620,6 @@ check_sys_bbrplus(){
 
 #检查安装Lotsever的系统要求
 check_sys_Lotsever(){
-	check_version
 	if [[ "${release}" == "centos" ]]; then
 		if [[ ${version} == "6" ]]; then
 			kernel_version="2.6.32-504"
@@ -673,74 +660,99 @@ check_sys_Lotsever(){
 	fi
 }
 
-check_status(){
-	kernel_version=`uname -r | awk -F "-" '{print $1}'`
-	kernel_version_full=`uname -r`
-	if [[ ${kernel_version_full} = "4.14.91-bbrplus" ]]; then
-		kernel_status="BBRplus"
-	elif [[ ${kernel_version} = "3.10.0" || ${kernel_version} = "3.16.0" || ${kernel_version} = "3.2.0" || ${kernel_version} = "4.4.0" || ${kernel_version} = "3.13.0"  || ${kernel_version} = "2.6.32" ]]; then
-		kernel_status="Lotserver"
-	elif [[ `echo ${kernel_version} | awk -F'.' '{print $1}'` == "4" ]] && [[ `echo ${kernel_version} | awk -F'.' '{print $2}'` -ge 9 ]]; then
-		kernel_status="BBR"
-	else 
-		kernel_status="noinstall"
-	fi
 
-	if [[ ${kernel_status} == "Lotserver" ]]; then
-		if [[ -e /appex/bin/serverSpeeder.sh ]]; then
-			run_status=`bash /appex/bin/serverSpeeder.sh status | grep "ServerSpeeder" | awk  '{print $3}'`
-			if [[ ${run_status} = "running!" ]]; then
-				run_status="启动成功"
-			else 
-				run_status="启动失败"
-			fi
-		else 
-			run_status="未安装加速模块"
-		fi
-	elif [[ ${kernel_status} == "BBR" ]]; then
-		run_status=`grep "net.ipv4.tcp_congestion_control" /etc/sysctl.conf | awk -F "=" '{print $2}'`
-		if [[ ${run_status} == "bbr" ]]; then
-			run_status=`lsmod | grep "bbr" | awk '{print $1}'`
-			if [[ ${run_status} == "tcp_bbr" ]]; then
-				run_status="BBR启动成功"
-			else 
-				run_status="BBR启动失败"
-			fi
-		elif [[ ${run_status} == "tsunami" ]]; then
-			run_status=`lsmod | grep "tsunami" | awk '{print $1}'`
-			if [[ ${run_status} == "tcp_tsunami" ]]; then
-				run_status="BBR魔改版启动成功"
-			else 
-				run_status="BBR魔改版启动失败"
-			fi
-		elif [[ ${run_status} == "nanqinlang" ]]; then
-			run_status=`lsmod | grep "nanqinlang" | awk '{print $1}'`
-			if [[ ${run_status} == "tcp_nanqinlang" ]]; then
-				run_status="暴力BBR魔改版启动成功"
-			else 
-				run_status="暴力BBR魔改版启动失败"
-			fi
-		else 
-			run_status="未安装加速模块"
-		fi
-	elif [[ ${kernel_status} == "BBRplus" ]]; then
-		run_status=`grep "net.ipv4.tcp_congestion_control" /etc/sysctl.conf | awk -F "=" '{print $2}'`
-		if [[ ${run_status} == "bbrplus" ]]; then
-			run_status=`lsmod | grep "bbrplus" | awk '{print $1}'`
-			if [[ ${run_status} == "tcp_bbrplus" ]]; then
-				run_status="BBRplus启动成功"
-			else 
-				run_status="BBRplus启动失败"
-			fi
-		else 
-			run_status="未安装加速模块"
-		fi
+
+
+
+####开始菜单####
+mainControl(){
+	clear
+	echo
+	echo -e "TCP加速 一键安装管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}"
+	echo
+	echo -e "${Green_font_prefix}0.${Font_color_suffix} 升级脚本"
+	echo -e "————————————内核管理————————————"
+	echo -e "${Green_font_prefix}1.${Font_color_suffix} 安装 BBR 内核"
+	echo -e "${Green_font_prefix}2.${Font_color_suffix} 安装 BBRmod 内核"
+	echo -e "${Green_font_prefix}3.${Font_color_suffix} 安装 BBRplus 内核" 
+	echo -e "${Green_font_prefix}4.${Font_color_suffix} 安装 Lotserver 内核[锐速]"
+	echo -e "————————————加速管理————————————"
+	echo -e "${Green_font_prefix}5.${Font_color_suffix} 开启 BBR 加速"
+	echo -e "${Green_font_prefix}6.${Font_color_suffix} 开启 BBRmod 加速"
+	echo -e "${Green_font_prefix}7.${Font_color_suffix} 开启 BBRmod Pro 加速[不支持部分系统]"
+	echo -e "${Green_font_prefix}8.${Font_color_suffix} 开启 BBRplus 加速"
+	echo -e "${Green_font_prefix}9.${Font_color_suffix} 开启 Lotserver 加速[锐速]"
+	echo -e "————————————杂项管理————————————"
+	echo -e "${Green_font_prefix}10.${Font_color_suffix} 卸载全部加速"
+	echo -e "${Green_font_prefix}11.${Font_color_suffix} 优化系统配置"
+	echo -e "${Green_font_prefix}12.${Font_color_suffix} 退出脚本"
+	echo -e "————————————————————————————————"
+	echo
+
+	checkStatus
+	if [[ ${kernel_status} == "noinstall" ]]; then
+		echo -e " 当前状态: ${Green_font_prefix}未安装${Font_color_suffix} 加速内核 ${Red_font_prefix}请先安装内核${Font_color_suffix}"
+	else
+		echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} ${_font_prefix}${kernel_status}${Font_color_suffix} 加速内核 , ${Green_font_prefix}${run_status}${Font_color_suffix}"
+		
 	fi
+echo
+read -p " 请输入数字 [0-11]:" num
+case "$num" in
+	0)
+	Update_Shell
+	;;
+	1)
+	check_sys_bbr
+	;;
+	2)
+	check_sys_bbrmod
+	;;
+	3)
+	check_sys_bbrplus
+	;;
+	4)
+	check_sys_Lotsever
+	;;
+	5)
+	startbbr
+	;;
+	6)
+	startbbrmod
+	;;
+	7)
+	startbbrmod_nanqinlang
+	;;
+	8)
+	startbbrplus
+	;;
+	9)
+	startlotserver
+	;;
+	10)
+	remove_all
+	;;
+	11)
+	optimizing_system
+	;;
+	12)
+	exit 1
+	;;
+	*)
+	clear
+	echo -e "${Error}:请输入正确数字 [0-12]"
+	sleep 5s
+	mainControl
+	;;
+esac
 }
 
-#############系统检测组件#############
-check_sys
-check_version
-[[ ${release} != "debian" ]] && [[ ${release} != "ubuntu" ]] && [[ ${release} != "centos" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} !" && exit 1
-start_menu
-
+####系统检测组件####
+checkRelease
+checkReleaseVersion
+if [[ ${release} == "debian" || ${release} == "ubuntu" || ${release} == "centos" ]]; then
+	mainControl
+else
+	echo -e "${Error} 本脚本不支持当前系统 ${release} !"
+	exit 1
+fi
