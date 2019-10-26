499f3e32abbf54f2630a918baa961d5d89573639
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2019-10-26 15:03:50 +0800

    Update onefast.sh

--- onefast.sh
+++ onefast.sh
@@ -131,7 +131,7 @@ installbbr(){
 		#yum install -y http://${github}/bbr/${release}/${version}/${bit}/kernel-ml-${kernel_version}.rpm
 		#yum remove -y kernel-headers
 		#yum install -y http://${github}/bbr/${release}/${version}/${bit}/kernel-ml-headers-${kernel_version}.rpm
-		#yum install -y http://${github}/bbr/${release}/${version}/${bit}/kernel-ml-devel-${kernel_version}.rpm
+		#yum install -y http://${github}/bbr/${release}/${version}/${bit}/kernel-ml-devel-${kernel_version}.rpm 
 		sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 		sudo rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
 		sudo yum --enablerepo=elrepo-kernel install kernel-ml -y
